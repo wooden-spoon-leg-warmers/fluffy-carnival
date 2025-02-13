e82259f91dc0478495bb1f15d91439bba19daa5d
wooden-spoon-leg-warmers <ejrickaby+temp@gmail.com>
2025-02-13 14:28:05 +1030

    fixed seeders not ignoring if already exists

--- api/seeders/seeder.py
+++ api/seeders/seeder.py
@@ -9,18 +9,20 @@ DATABASE_URL = os.getenv('DATABASE_URL', 'postgres://api:password@postgresql.dat
 conn = psycopg2.connect(DATABASE_URL)
 cur = conn.cursor()
 
-# Insert data into mytable
+# Insert data into mytable with ON CONFLICT clause
 cur.execute("""
     INSERT INTO mytable (id, name, created_at) VALUES
     ('1', 'Sample Name 1', %s),
-    ('2', 'Sample Name 2', %s);
+    ('2', 'Sample Name 2', %s)
+    ON CONFLICT (id) DO NOTHING;
 """, (datetime.now(), datetime.now()))
 
-# Insert data into othertable
+# Insert data into othertable with ON CONFLICT clause
 cur.execute("""
     INSERT INTO othertable (id, name, created_at) VALUES
     ('1', 'Other Sample Name 1', %s),
-    ('2', 'Other Sample Name 2', %s);
+    ('2', 'Other Sample Name 2', %s)
+    ON CONFLICT (id) DO NOTHING;
 """, (datetime.now(), datetime.now()))
 
 # Commit changes and close connection
